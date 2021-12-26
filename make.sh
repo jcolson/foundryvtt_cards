9aa5f1a3219b4427e710e2c87d8513d376b511b8
Jay Colson <jay@karma.net>
2021-12-26 18:51:35 +0000

    not sure how the update was removed ... re-added

--- make.sh
+++ make.sh
@@ -7,8 +7,8 @@ cd Great-Dalmuti
 zip -ur ../FoundryVTTCards.zip images
 cd ..
 cd 54-Playing-Cards
-zip -r ../FoundryVTTCards.zip images
+zip -ur ../FoundryVTTCards.zip images
 cd ..
 cd Three-Dragon-Ante
-zip -r ../FoundryVTTCards.zip images
+zip -ur ../FoundryVTTCards.zip images
 cd ..
